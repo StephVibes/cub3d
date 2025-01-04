db97ed1d0871a9a369b8b1b93b382f931689ce95
Steph <stephanie.manriq@gmail.com>
2025-01-04 13:17:56 +0100

    working in init the minilibx

--- srcs/image/init.c
+++ srcs/image/init.c
@@ -1,5 +1,26 @@
 #include "cub3d.h"
 
-void maze_init()
+void maze_init(t_maze *maze)
 {
+	maze -> mlx_ptr = mlx_init(); // connect with the minilibx
+	if (maze -> mlx_ptr == NULL)
+		error(); // improve
+	maze -> win_ptr = mlx_new_window(maze -> mlx_ptr, WIDTH, HEIGHT, "Cub3D"); // create new window
+	if (maze -> win_ptr == NULL)
+	{
+		mlx_destroy_display(maze -> mlx_ptr);
+		free(maze -> mlx_ptr);
+		error(); // improve
+	}
+	maze -> screen.img_ptr = mlx_new_image(maze -> mlx_ptr, WIDTH, HEIGHT); // create image
+	if (maze -> screen.img_ptr == NULL)
+	{
+		mlx_destroy_window(maze -> mlx_ptr, maze -> win_ptr);
+		mlx_destroy_display(maze -> mlx_ptr);
+		free(maze -> mlx_ptr);
+		error(); // improve
+	}
+	maze -> screen.data = mlx_get_data_addr(maze -> screen.img_ptr, &maze -> screen.bpp, &maze -> screen.line_length, &maze -> screen.endian);
+	//events_init()?
+	//data_init()?
 }
