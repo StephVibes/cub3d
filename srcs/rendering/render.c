377d4261e99b0588ab3c07483789c342c22fc138
Steph <stephanie.manriq@gmail.com>
2025-03-29 19:35:11 +0100

    Fixing corners in process

--- srcs/rendering/render.c
+++ srcs/rendering/render.c
@@ -147,22 +147,16 @@ int array_equals(int arr[], int expected[])
     else
         ray->coord = 0;
 }*/
+
+
 void def_coord(t_ray *ray, t_maze *maze)
 {
-    if (ray->angle >= 0 && ray->angle < M_PI/2)
-            ray->coord = 3;
-        else if(ray->angle >= M_PI/2 && ray->angle < M_PI)
-            ray->coord = 2;
-        else if (ray->angle >= M_PI && ray->angle < 3 * M_PI / 2)
-            ray->coord = 1;
-        else if (ray->angle >= 3 * M_PI / 2 && ray->angle < 2 * M_PI)
-            ray->coord = 0;
 
     (void)maze;
-    //double prev_x = ray->hit_point.x - cos(ray->angle);
-    //double prev_y = ray->hit_point.y - sin(ray->angle);
+    double prev_x = ray->hit_point.x - cos(ray->angle);
+    double prev_y = ray->hit_point.y - sin(ray->angle);
 
-    /* int wall_facing_north[4] = {0, 1, 1, 1};    // Only North wall
+    int wall_facing_north[4] = {0, 1, 1, 1};    // Only North wall
     int wall_facing_south[4] = {1, 0, 1, 1};    // Only South wall
     int wall_facing_east[4] = {1, 1, 0, 1};     // Only East wall
     int wall_facing_west[4] = {1, 1, 1, 0};     // Only West wall
@@ -174,8 +168,8 @@ void def_coord(t_ray *ray, t_maze *maze)
     else if (array_equals(ray->compass, wall_facing_east))
         ray->coord = 2;
     else if (array_equals(ray->compass, wall_facing_west))
-        ray->coord = 3; */
-    /* if (!touch(prev_x, ray->hit_point.y, maze) && !touch(ray->hit_point.x, prev_y, maze))
+        ray->coord = 3;
+    else if (!touch(prev_x, ray->hit_point.y, maze) && !touch(ray->hit_point.x, prev_y, maze))
     {
         if (ray->angle >= 0 && ray->angle < M_PI/2)
             ray->coord = 3;
@@ -185,9 +179,8 @@ void def_coord(t_ray *ray, t_maze *maze)
             ray->coord = 1;
         else if (ray->angle >= 3 * M_PI / 2 && ray->angle < 2 * M_PI)
             ray->coord = 0;
-    } */
+    }
 }
-
 
 void hit_compass(t_ray *ray, t_maze *maze)
 {
